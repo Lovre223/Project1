b723c00ec25800be142a139c14d27bd1b024dffe
Lovre Burazer-Pavešković <lburazer8@gmail.com>
2022-11-23 15:20:52 +0100

    Create uprava.sql

---
+++ uprava.sql
@@ -0,0 +1,28 @@
+drop database if exists uprava;
+create database uprava;
+use uprava;
+
+
+create table zupanija(
+    sifra int not null primary key auto_increment,
+    naziv varchar(20),
+    zupan varchar(20)
+);
+
+
+create table opcina(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    zupanija int
+);
+
+
+
+
+alter table opcina add foreign key (zupanija) references zupanija(sifra);
+
+insert into opcina (sifra, naziv,zupanija)
+values (null, 'Vrpolje', 1);
+
+insert into zupanija (sifra, naziv, zupan)
+values (null, 'Sibensko-kninska', 'Zeljko Buric');
