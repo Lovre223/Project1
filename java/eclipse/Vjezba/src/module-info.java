0e04f720a89b9244a2758fe797a76bc03d515977
Lovre Burazer-Pavešković <lburazer8@gmail.com>
2022-12-19 19:38:24 +0100

    Zadaci za vježbu

---
+++ java/eclipse/Vjezba/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author lovre
+ *
+ */
+module Vjezba {
+	
+	requires java.desktop;
+}
---
+++ java/eclipse/Vjezba/src/vjezba/vjezba1.java
@@ -0,0 +1,15 @@
+package vjezba;
+
+import javax.swing.JOptionPane;
+
+public class vjezba1 {
+	
+	public static void main(String[] args) {
+		
+		int i = Integer.parseInt(JOptionPane.showInputDialog("Upisi dva broja: "));
+		
+		System.out.println(i);
+		
+	}
+
+}
