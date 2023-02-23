1933d18fd4f78694764dfab34d42924b64c49e63
Lovre Burazer-Pavešković <lburazer8@gmail.com>
2023-02-23 22:07:51 +0100

    Vježbanje

--- java/eclipse/Nizovi/src/edunova/Z04.java
+++ java/eclipse/Nizovi/src/edunova/Z04.java
@@ -2,10 +2,30 @@ package edunova;
 
 public class Z04 {
 	public static void main(String[] args) {
+
+		int niz[] = { 25, 38, 42, 3, 2, 23, 1 };
+
+		System.out.println(" 38 je na položaju " + pronadiIndex(niz, 38));
 		
 		
 		
+	}
+
+	private static int pronadiIndex(int[] niz, int br) {
 		
+		for(int i = 0; i<niz.length;i++) {
+			
+			if(niz[i] == br) {
+				
+				
+				return (i+1);
+			}
+			
+			
+		}
+		
+		
+		return -1;
 	}
 
 }
